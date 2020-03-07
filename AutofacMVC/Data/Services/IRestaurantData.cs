e217bdb36cb7ac8f442cabe2aa2710b69963f64a
Luis Andrade <l.andrade.l@hotmail.com>
2020-03-06 22:39:18 -0500

    Initial solutions implementations

---
+++ AutofacMVC/Data/Services/IRestaurantData.cs
@@ -0,0 +1,44 @@
+﻿using Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Services
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAll();
+        Restaurant Get(int id);
+        void Add(Restaurant restaurant);
+    }
+
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> restaurants;
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>() {
+                new Restaurant() { Id = 1, Name = "Le restarant", Cuisine = CuisineType.French },
+                new Restaurant() { Id = 2, Name = "Il ristaurante", Cuisine = CuisineType.Italian },
+            };
+        }
+
+        public void Add(Restaurant restaurant)
+        {
+            restaurants.Add(restaurant);
+            restaurant.Id = restaurants.Max(x => x.Id) + 1;
+        }
+
+        public Restaurant Get(int id)
+        {
+            return restaurants.FirstOrDefault(x => x.Id == id);
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return restaurants.OrderBy(x => x.Name);
+        }
+    }
+}
---
+++ Fundamentos/ConsoleApp/AppProject/Program.cs
@@ -0,0 +1,76 @@
+﻿using AppProject.Modules;
+using System;
+
+
+namespace AppProject
+{
+    public delegate void InitModule();
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Recommended Modules order
+            // 1. Fundamentals
+            // 2. Lambda
+            // 3. Delegates
+            // 4. TaskParallel
+
+
+            #region [Fundamentals]
+
+            //using (var module = new Fundamentals())
+            //{
+            //    module.Init();
+            //}
+
+            #endregion
+
+            #region [Lambda]
+
+            //using (var module = new Lambda())
+            //{
+            //    module.Init();
+            //}
+
+            #endregion
+
+            #region [TaskParallel]
+
+            using (var module = new BasicTasks())
+            {
+                module.Init();
+            }
+
+            #endregion
+
+            #region [Delegates]
+
+            //using (var module = new BasicDelegates())
+            //{
+            //    module.Init();
+            //}
+
+            #endregion
+
+            #region [LinQ]
+
+            //using (var module = new BasicLinQ())
+            //{
+            //    module.Init();
+            //}
+
+            #endregion
+
+            #region [Files]
+
+            //using (var module = new BasicFiles())
+            //{
+            //    module.Init();
+            //}
+
+            #endregion
+
+            Console.ReadLine();
+        }
+    }
+}
